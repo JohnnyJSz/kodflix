2cdf88fd22c7b7afa90c66ff5412bc76a8db2dc9
Jonathan J S <jonatelo.92@gmail.com>
2019-10-29 21:10:38 +0000

    movie title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -15,16 +15,41 @@ function App() {
           <div className='titles'>
             <img src={blackMirrorCover} alt='Black Mirror' />
             <div className='overlayTitle'>
-              <h3>Black Mirror</h3>
+              <h1>Black Mirror</h1>
             </div>
           </div>
-          <div className='titles'><img src={breakingBadCover} alt='Breaking Bad' /></div>
-          <div className='titles'><img src={deathNoteCover} alt='Death Note' /></div>
+          <div className='titles'>
+            <img src={breakingBadCover} alt='Breaking Bad' />
+            <div className='overlayTitle'>
+              <h1>Breaking Bad</h1>
+            </div>
+          </div>
+          <div className='titles'>
+            <img src={deathNoteCover} alt='Death Note' />
+            <div className='overlayTitle'>
+              <h1>Death Note</h1>
+            </div>
+          </div>
         </div>
         <div className='row2'>
-          <div className='titles'><img src={gameOfThronesCover} alt='Game Of Thrones' /></div>
-          <div className='titles'><img src={theWalkingDeadCover} alt='The Walking Dead' /></div>
-          <div className='titles'><img src={ironManCover} alt='Iron Man 2' /></div>
+          <div className='titles'>
+            <img src={gameOfThronesCover} alt='Game Of Thrones' />
+            <div className='overlayTitle'>
+              <h1>Game Of Thrones</h1>
+            </div>
+          </div>
+          <div className='titles'>
+            <img src={theWalkingDeadCover} alt='The Walking Dead' />
+            <div className='overlayTitle'>
+              <h1>The Walking Dead</h1>
+            </div>
+          </div>
+          <div className='titles'>
+            <img src={ironManCover} alt='Iron Man 2' />
+            <div className='overlayTitle'>
+              <h1>Iron Man 2</h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
