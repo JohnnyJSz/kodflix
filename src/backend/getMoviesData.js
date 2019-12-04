a73ccc3e9a70e45ec35c269ec518e13cd6c8c78a
Jonathan J S <jonatelo.92@gmail.com>
2019-12-04 11:01:17 +0000

    return a list of shows from backend #19

---
+++ src/backend/getMoviesData.js
@@ -0,0 +1,25 @@
+// import blackMirrorCover from '../images/blackMirrorCover.jpg';
+// import breakingBadCover from '../images/breakingBadCover.jpg';
+// import deathNoteCover from '../images/deathNoteCover.jpg';
+// import gameOfThronesCover from '../images/gameOfThronesCover.jpg';
+// import theWalkingDeadCover from '../images/theWalkingDeadCover.jpg';
+// import ironMan2Cover from '../images/ironMan2Cover.jpg';
+// import spiderManHomecomingCover from '../images/spiderManCover.jpg';
+// import blackPantherCover from '../images/blackPantherCover.jpg';
+//
+// cover: ironMan2Cover
+
+function getMoviesData(){
+    return [
+      {title: 'Black Mirror', id: 'blackMirror', synopsis: 'An anthology series exploring a twisted, high-tech world where humanity\'s greatest innovations and darkest instincts collide.'},
+      {title: 'Breaking Bad', id: 'breakingBad', synopsis: 'A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family\'s future.'},
+      {title: 'Death Note', id: 'deathNote', synopsis: 'A high school student named Light Turner discovers a mysterious notebook that has the power to kill anyone whose name is written within its pages, and launches a secret crusade to rid the world of criminals.'},
+      {title: 'Game of Thrones', id: 'gameOfThrones', synopsis: 'Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years.'},
+      {title: 'The Walking Dead', id: 'theWalkingDead', synopsis: 'Sheriff Deputy Rick Grimes wakes up from a coma to learn the world is in ruins and infested by zombies. He must lead a group of survivors to stay alive.'},
+      {title: 'Iron Man 2', id: 'ironMan2', synopsis: 'With the world now aware of his identity as Iron Man, Tony Stark must contend with both his declining health and a vengeful mad man with ties to his father\'s legacy.'},
+      {title: 'Spider Man Homecoming', id: 'spiderManHomeoming', synopsis: 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.'},
+      {title: 'Black Panther', id: 'blackPanther', synopsis: 'T\'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country\'s past.'}
+    ];
+  }
+
+module.exports = getMoviesData();
