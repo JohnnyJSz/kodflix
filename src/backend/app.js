3e937460c583c2907ce6081ad5a3198bda52f0bf
Jonathan J S <jonatelo.92@gmail.com>
2019-12-09 09:25:08 +0000

    improve setup for backend server #20

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 const getMoviesData = require('./getMoviesData');
+app.get('/rest/moviesData', (req, res) => res.send(getMoviesData));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-app.get('/rest/moviesData', (req, res) => res.send(getMoviesData))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
