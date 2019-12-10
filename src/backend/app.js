d0c7ca5c7064820f0e37eb8ada63729933ba8376
Jonathan J S <jonatelo.92@gmail.com>
2019-12-10 15:22:47 +0000

    reorder express routes

--- src/backend/app.js
+++ src/backend/app.js
@@ -8,9 +8,8 @@ app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('/rest/moviesData', (req, res) => res.send(getMoviesData));
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
